13a1869fe6af247b29d56581c2c392399c70f81e
Chris Aquino <chris.aquino@gmail.com>
2018-12-06 13:46:32 -0500

    l33tsp34ks that mofo

--- src/OutputBox.js
+++ src/OutputBox.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+const l33t = {
+    A: '4',
+    E: '3',
+    G: '6',
+    I: '1',
+    O: '0',
+    S: '5',
+    T: '7',
+}
+
+function toL33t(originalText) {
+    const letterArray = originalText.split('');
+    const l33tArray = letterArray.map(letter => {
+        return l33t[letter.toUpperCase()] || letter;
+    });
+    return l33tArray.join('');
+}
+
 const OutputBox = (props) => {
     return (
         <div className='output-box'>
-            {props.theText.toUpperCase()}
+            {toL33t(props.theText)}
         </div>        
     );
 };
