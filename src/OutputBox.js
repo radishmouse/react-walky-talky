731fe39f97f94a9754ef1f6ab97cfbda3c94e274
Chris Aquino <chris.aquino@gmail.com>
2018-12-06 14:44:14 -0500

    uses ternary operator in OutputBox

--- src/OutputBox.js
+++ src/OutputBox.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const OutputBox = (props) => {
 
-    let textToDisplay = props.theText;
-    if (props.transformFunction) {
-        textToDisplay = props.transformFunction(props.theText);
-    }
+    // let textToDisplay = props.theText;
+    // if (props.transformFunction) {
+    //     textToDisplay = props.transformFunction(props.theText);
+    // }
+
+    let textToDisplay = props.transformFunction ? props.transformFunction(props.theText)
+                                                : props.theText
 
     return (
         <div className='output-box'>
